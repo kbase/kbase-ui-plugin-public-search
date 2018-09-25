0a542e1684ef1e0246c1ebc46d08f0a4238c4a1b
Erik Pearson <eapearson@lbl.gov>
2018-09-25 09:22:01 -0700

    add dagre and d3 as deps [SCT-1235] - not being used yet, just getting ready

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -5,11 +5,29 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         copy: {
-            nodeModules: {
+            vega: {
                 expand: true,
                 flatten: true,
                 src: 'node_modules/vega-lib/build/vega.js',
                 dest: '../src/plugin/iframe_root/js/vendor/vega'
+            },
+            d3: {
+                expand: true,
+                flatten: true,
+                src: 'node_modules/d3/dist/d3.js',
+                dest: '../src/plugin/iframe_root/js/vendor/d3'
+            },
+            dagre: {
+                expand: true,
+                flatten: true,
+                src: 'node_modules/dagre/dist/dagre.js',
+                dest: '../src/plugin/iframe_root/js/vendor/dagre'
+            },
+            'dagre-d3': {
+                expand: true,
+                flatten: true,
+                src: 'node_modules/dagre-d3/dist/dagre-d3.js',
+                dest: '../src/plugin/iframe_root/js/vendor/dagre-d3'
             }
         }
     });
